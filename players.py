e6868a873842fc81d712baec5a90642cf4d241cc
AndrewManHayChiu <amhchiu@gmail.com>
2022-04-19 00:29:41 +1000

    created basic player class

---
+++ players.py
@@ -0,0 +1,13 @@
+
+
+class Player:
+    """a player"""
+    
+    def __init__(self, name):
+        self.name = name
+        self.gender = 'male'
+        self.tier = 7
+
+test_player = Player(name='Andrew')
+test_player.name
+test_player.tier

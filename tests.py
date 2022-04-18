71fbe7b024c63718a2972c3ab146f2754de62661
AndrewManHayChiu <amhchiu@gmail.com>
2022-04-19 00:54:49 +1000

    add test scenario; forgot to use test branch

--- tests.py
+++ tests.py
@@ -30,3 +30,18 @@ test_player.name
 test_player.tier
 test_player.tier = 'shit'
 test_player.tier
+
+# Create a new session and add players to session
+test_session = Session(weekday='Tuesday', session='6:30')
+
+# Players are created when they sign in.
+andrew = Player(name='Andrew')
+test_session.add_player(andrew)
+felix = Player(name='Felix')
+test_session.add_player(felix)
+
+# Can access player details
+test_session.players[0].name
+test_session.players[0].tier
+
+# Games can be created

1b4016e6a43163f7c7d3fac54cf3dbee50d17298
Andrew Chiu <amhchiu@gmail.com>
2022-04-21 11:53:32 +1000

    Update test script

--- tests/tests.py
+++ tests/tests.py
@@ -38,50 +38,26 @@ player.tier
 
 # Create a new session and add players to session
 test_session = Session(weekday='Tuesday', session='6:30')
+test_session.info(verbose=True)
 
 # Add players using Player id
 test_session.add_player(Player(player_id=1))
 test_session.add_player(Player(player_id=2))
 test_session.add_player(Player(player_id=3))
 test_session.add_player(Player(player_id=4))
+test_session.add_player(Player(player_id=5))
 
 # Can access player details
-test_session.players[0].name
-test_session.players[0].tier
-test_session.players[1].name
-test_session.players[1].tier
-
-# Games can be created
-test_session.add_game(test_game)
-test_session.games[0]
+# test_session.players[0].name
+# test_session.players[0].tier
+# test_session.players[1].name
+# test_session.players[1].tier
 
 # Print session information
 test_session.info()
 test_session.info(verbose=True)
 
-# Test db
-conn = sqlite3.connect('test.db')
-cur = conn.cursor()
-for row in cur.execute("""
-            SELECT name 
-            FROM sqlite_schema 
-            WHERE type = 'table' AND name NOT LIKE 'sqlite_%';
-            """):
-    print(row)
-
-for row in cur.execute("""PRAGMA table_info([players]);"""):
-    print(row)
-
-conn = sqlite3.connect('test.db')
-cursor = conn.cursor()
-print("Connected to SQLite")
-cursor.execute("""SELECT * FROM player_tiers""")
-records = cursor.fetchall()
-len(records)
-for row in records:
-    print(row)
-
-cursor.close()
-
-# cursor.execute("""DROP TABLE player_tiers""")
+# Games can be created
+test_session.add_game(test_game)
+test_session.games[0]
 

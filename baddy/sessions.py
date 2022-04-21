52d07cc12baba39a4d3de831647de755e6810be2
Andrew Chiu <amhchiu@gmail.com>
2022-04-21 14:31:28 +1000

    Game class added to sessions.py; Session.add_game automatically allocates first 4 in queue to a game and moves them to the back of the queue.

--- baddy/sessions.py
+++ baddy/sessions.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 class Session:
     """a baddy session"""
     
@@ -5,7 +7,7 @@ class Session:
         self.weekday = weekday
         self.session = session
         self.players = []
-        self.games = []
+        self.games = pd.DataFrame({'type': [], 'players': []})
         self.courts = 6
         self.doubles = True
         self.points = 21
@@ -25,27 +27,74 @@ class Session:
         else:
             print('Player not in the queue.')
     
-    def add_game(self, game):
-        self.games.append(game)
+    def add_game(self, game=None, auto=True):
+        if auto:
+            game = Game(queue=self.queue, auto=auto)
+        else:
+            pass
+                     
+        self.games = pd.concat([self.games,
+                                pd.DataFrame({
+                                    'type': [game.game_type], 
+                                    'players': [game.players]})])
         
+        # Move players to the back of the queue
+        for player in game.players:
+            self.queue.remove(player)
+            self.queue.append(player)
+    
     def info(self, verbose=False):
-        print('Session:', self.weekday, self.session)
+        print('SESSION INFO:')
         if verbose == True:
             print('    Venue:', self.venue)
+            print('    Time:', self.weekday, self.session)
             print('    Courts:', self.courts)
             print('    Floor type:', self.floor_type)
             print('    Points:', self.points)
             print('    Doubles:', self.doubles)
             print('    Skill levels:', self.skill_levels)
-        print()
+            print()
         
         print('Players:')
         for count, player in enumerate(self.players, start=1):
             print('    ', count, player.name, '(' + str(player.tier) +')')
         print()
         
-        print('Queue:')
-        print(self.queue)
+        print('Games played:', len(self.games))
         print()
         
-        print('Games:')
+        print('Queue:')
+        print(self.queue)
+
+
+class Game:
+    """
+        A baddy game
+        By default, MD, and automatic allocation from Session.queue
+        
+        game_types:
+            - MD: Men's Doubles
+            - WD: Women's Doubles
+            - XD: Mixed Doubles
+    """
+    
+    def __init__(self, queue, auto=True):
+        self.players = []
+        self.doubles = True
+        self.game_type = "MD"
+        # self.team_1 = []
+        # self.team_2 = []
+        # self.team_1_score = 21 # team 1 wins by default; can override this value
+        # self.team_2_score = ''
+
+        if auto:
+            self.players.extend(queue[0:4])
+        else:
+            pass
+    
+    def info(self):
+        print('GAME INFO')
+        print('Type:', self.game_type)
+        print('Players:')
+        for count, player in enumerate(self.players, start=1):
+            print('    ', count, player)
--- tests/tests.py
+++ tests/tests.py
@@ -48,10 +48,20 @@ test_session.add_player(Player(player_id=6))
 # Print session information
 test_session.info(verbose=True)
 
+# Games can be created automatically
+test_session.add_game(auto=True)
+test_session.games
+
+# And players are moved to the back of the Queue
+test_session.info()
+
+# Games can be also be created manually
+
 # Player leaves; remove from queue
-test_session.queue
-test_session.remove_player(player_name='David Ng')
-test_session.queue
+# test_session.queue
+# test_session.remove_player(player_name='David Ng')
+# test_session.queue
+# test_session.remove_player(player_name='David Ng')
 
 # Can access player details
 # test_session.players[0].name
@@ -59,9 +69,4 @@ test_session.queue
 # test_session.players[0].gender
 # test_session.players[1].name
 # test_session.players[1].tier
-test_session.players
-
-# Games can be created
-test_session.add_game(test_game)
-test_session.games[0]
-
+# test_session.players

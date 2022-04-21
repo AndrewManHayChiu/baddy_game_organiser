3065e91349ebace53768bf7ee3a0ea1ce2b64daa
Andrew Chiu <amhchiu@gmail.com>
2022-04-21 10:25:50 +1000

    create setup.py file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from disutils.core import setup
+
+setup(
+    name='baddy_game_organiser',
+    version='0.1dev',
+    packages=['baddy', ],
+    license='MIT License',
+    long_description=open('README.md').read()
+)

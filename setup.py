bb3c7e455fdd172be72e6673373e87d40560f3b2
Andrew Chiu <amhchiu@gmail.com>
2022-04-21 10:28:00 +1000

    update setup.py with additional information

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@ from disutils.core import setup
 setup(
     name='baddy_game_organiser',
     version='0.1dev',
+    author='Andrew Man Hay Chiu',
     packages=['baddy', ],
     license='MIT License',
-    long_description=open('README.md').read()
+    long_description=open('README.md').read(),
+    python_requires='>=3.7'
 )
